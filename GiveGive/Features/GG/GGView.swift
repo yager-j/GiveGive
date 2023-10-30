9e07a2f2d6cef5263f778153668cea85754409aa
yager-j <joanneyager@gmail.com>
2023-10-30 16:25:28 +0100

    Add GG View with button to open sheet where GG is fed by scanning toys

---
+++ GiveGive/Features/GG/GGView.swift
@@ -0,0 +1,77 @@
+//
+//  GGView.swift
+//  GiveGive
+//
+//  Created by Joanne Yager on 2023-10-30.
+//
+
+import SwiftUI
+import RealityKit
+
+struct GGView: View {
+    
+    @State private var showSheet = false
+
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            AnimationView()
+            FeedButton(showSheet: $showSheet)
+        }
+        .ignoresSafeArea()
+        .sheet(isPresented: $showSheet) {
+            Text("Hello")
+        }
+    }
+}
+
+struct AnimationView: View {
+    var body: some View {
+        ZStack {
+            ARViewContainer()
+        }
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero, cameraMode: .nonAR, automaticallyConfigureSession: true)
+        
+        arView.environment.lighting.intensityExponent = 3
+        let newAnchor = AnchorEntity(world: .zero)
+        let newEnt = try! Entity.load(named: "gg")
+        newAnchor.addChild(newEnt)
+        arView.scene.addAnchor(newAnchor)
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) { }
+}
+
+struct FeedButton: View {
+    
+    @Binding var showSheet: Bool
+    
+    var body: some View {
+        Button {
+            showSheet = true
+        } label: {
+            Image(systemName: "fork.knife")
+                .font(.title.weight(.semibold))
+                .padding()
+                .background(Color.pink)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .shadow(radius: 4, x: 0, y: 4)
+        }
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 32, trailing: 32))
+    }
+}
+
+#Preview {
+    GGView()
+}
+
+
--- GiveGive/GiveGiveApp.swift
+++ GiveGive/GiveGiveApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GiveGiveApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GGView()
         }
     }
 }
