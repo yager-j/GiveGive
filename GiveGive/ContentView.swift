50e5deaeb49a5572b2c0949e428616d76f457f9c
joyagerr <joanneyager@gmail.com>
2023-10-30 13:31:44 +0100

    Initial Commit

---
+++ GiveGive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GiveGive
+//
+//  Created by Joanne Yager on 2023-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GiveGive/GiveGiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiveGiveApp.swift
+//  GiveGive
+//
+//  Created by Joanne Yager on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct GiveGiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
