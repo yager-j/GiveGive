e2e3b3366adacba56edab5e9432701d512bac709
yager-j <joanneyager@gmail.com>
2023-10-30 14:02:39 +0100

    Add WorkSans custom fonts

--- GiveGive/ContentView.swift
+++ GiveGive/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello!")
+                .font(Font.custom("WorkSans-ExtraBold", size: 40))
         }
         .padding()
     }
