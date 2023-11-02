4a4de078b5dccf5d9b49839de8b9f4cb3a96f81b
yager-j <joanneyager@gmail.com>
2023-11-02 15:27:35 +0100

    Add Firebase anonymous sign in

--- GiveGive/GiveGiveApp.swift
+++ GiveGive/GiveGiveApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct GiveGiveApp: App {
+    
+    // Register app delegate for Firebase setup
+        @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
+            let authViewModel = AuthViewModel()
             GGView()
+                .environmentObject(authViewModel)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
